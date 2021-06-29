a10d5bd6d277897bafe0d339e79cf720bcac97c2
vishal <vishalambekar459@gmail.com>
2021-06-29 22:59:59 +0530

    First commit for user service

---
+++ PhotoAppApiUsers/src/main/java/com/vishal/app/photoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -0,0 +1,16 @@
+package com.vishal.app.photoAppApiUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoAppApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+	}
+
+}
---
+++ PhotoAppApiUsers/src/test/java/com/vishal/app/photoAppApiUsers/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishal.app.photoAppApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
